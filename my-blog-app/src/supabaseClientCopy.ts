51edd0969ccb8702130c5d171c558e3b4afe1a4c
Tristan <tumbagatristanehron31@gmail.com>
2026-01-14 16:44:22 +0800

    delete

--- my-blog-app/src/supabaseClientCopy.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-//NOTE: rename this file to -> 'supabaseClient.ts'
-// Get these from your Supabase Dashboard -> Settings -> API
-const supabaseUrl = 'https://your-supabase-id.supabase.co';
-const supabaseAnonKey = 'your-supabase-anon-key';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
