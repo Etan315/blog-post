402e0f1bd84f9381ef0eb53f2e8094fbb783dd8c
Tristan <tumbagatristanehron31@gmail.com>
2026-01-14 16:31:38 +0800

    feat: changed path of env file

--- my-blog-app/vite.config.ts
+++ my-blog-app/vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url'
+import path from 'path'
 
-// https://vite.dev/config/
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
 export default defineConfig({
   plugins: [react()],
   base: './',
+  // This explicitly points to the folder ABOVE 'my-blog-app'
+  envDir: path.resolve(__dirname, '..'), 
 })
